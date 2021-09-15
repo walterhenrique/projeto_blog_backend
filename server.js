09cfafecebd9f3421dcee26630f47c2f099197f4
walterhenrique <56816256+walterhenrique@users.noreply.github.com>
2021-09-15 17:09:38 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+const axios = require('axios')
+
+const app = express()
+app.use(cors())
+app.get('/', async(req, res) =>{
+
+    try {
+        const { data } = await axios('https://jsonplaceholder.typicode.com/users')
+        //return res.json(data)
+        return res.json([{ name: 'jeff'},{ name: 'diego'}])
+        //nesta linha de cima, em vez de nomes, enviar a postagem salva
+    } catch (error) {
+        console.error(error)
+    }
+    }
+)
+
+app.listen('4567')
